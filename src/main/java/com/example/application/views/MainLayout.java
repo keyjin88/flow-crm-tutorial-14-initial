5f186134ebfbdd362cc5aef57d23ba704b663a81
AV <keyjin88@gmail.com>
2022-12-04 16:07:37 +0300

    Add secure

--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -1,20 +1,31 @@
 package com.example.application.views;
 
+import com.example.application.security.SecurityService;
 import com.example.application.views.list.ListView;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 
+@PWA(
+        name = "VaadinCRM",
+        shortName = "CRM",
+        offlinePath="offline.html",
+        offlineResources = { "./images/offline.png"}
+)
 @Theme(themeFolder = "flowcrmtutorial")
 public class MainLayout extends AppLayout {
+    private final SecurityService securityService;
 
-    public MainLayout() {
+    public MainLayout(SecurityService securityService) {
+        this.securityService = securityService;
         createHeader();
         createDrawer();
     }
@@ -23,12 +34,12 @@ public class MainLayout extends AppLayout {
         H1 logo = new H1("Vaadin CRM");
         logo.addClassNames("text-l", "m-m");
 
-        HorizontalLayout header = new HorizontalLayout(
-                new DrawerToggle(),
-                logo
-        );
+        Button logout = new Button("Log out", e -> securityService.logout());
+
+        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo, logout);
 
         header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.expand(logo);
         header.setWidth("100%");
         header.addClassNames("py-0", "px-m");
 
